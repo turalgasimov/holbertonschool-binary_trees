1644037a79060ae0946d58b4b60b7cf422e077fb
turalgasimov <turallgasimov@gmail.com>
2026-01-28 15:40:28 +0000

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,6 +19,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 
 	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
 	node->n = value;
 
 	return (node);
--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -26,6 +26,8 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
