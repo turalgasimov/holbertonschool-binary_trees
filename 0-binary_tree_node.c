2e54e09f6fe85cda4c59a4cd99b16c9d81e05ae1
turalgasimov <turallgasimov@gmail.com>
2026-01-28 16:06:06 +0000

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 	{
 		free(node);
