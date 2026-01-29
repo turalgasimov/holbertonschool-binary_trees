9c4aa2b5ae53c1805d39b0c0b5fa818406299f58
turalgasimov <turallgasimov@gmail.com>
2026-01-29 11:48:28 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,25 +11,23 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	if (!parent)
+	if (parent == NULL)
 		return (NULL);
 
 	node = malloc(sizeof(binary_tree_t));
-	if (!node)
-	{
-		free(node);
+	if (node == NULL)
 		return (NULL);
-	}
 
 	node->n = value;
 	node->parent = parent;
 	node->right = NULL;
 
-	if (parent->left)
+	if (parent->left != NULL)
 	{
 		node->left = parent->left;
 		parent->left->parent = node;
-	}
+	} else
+		node->left = NULL;
 
 	parent->left = node;
 	return (node);
