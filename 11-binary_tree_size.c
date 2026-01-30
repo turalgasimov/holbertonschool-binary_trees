b622ab49766b8e8e2573fe8a7585bdc0ee52dfee
turalgasimov <turallgasimov@gmail.com>
2026-01-30 15:31:36 +0000

    11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - entry point
+ * @tree: root
+ *
+ * Return: depth
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t sl, sr;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	sl = binary_tree_size(tree->left);
+	sr = binary_tree_size(tree->right);
+
+	return (sl + sr + 1);
+}
