3a9357558e5d5eb7282d07821deed90fd39f34cc
turalgasimov <turallgasimov@gmail.com>
2026-01-30 15:16:37 +0000

    10-binary_tree_depth.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t hl, hr;
 
 	if (!tree)
-		hl = hr = -1;
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (0);
 
 	hl = binary_tree_height(tree->left);
 	hr = binary_tree_height(tree->right);
