705d2fd521e7213fb43a2013706268d503d8dbcd
turalgasimov <turallgasimov@gmail.com>
2026-01-30 15:47:43 +0000

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,8 +13,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (1);
+
 	ll = binary_tree_leaves(tree->left);
 	rl = binary_tree_leaves(tree->right);
 
-	return (ll + rl + 1);
+	return (ll + rl);
 }
