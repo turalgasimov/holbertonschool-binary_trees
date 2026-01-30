14e5e3082660e6fee67046b3df48cf910130603e
turalgasimov <turallgasimov@gmail.com>
2026-01-30 15:32:32 +0000

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,9 +13,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (1);
-
 	sl = binary_tree_size(tree->left);
 	sr = binary_tree_size(tree->right);
 
