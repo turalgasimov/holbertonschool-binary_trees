b936572e142cbff3c36a4cf203a04c81105a223e
turalgasimov <turallgasimov@gmail.com>
2026-01-30 14:24:20 +0000

    7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - entry point
+ * binary_tree_inorder - entry point
  * @tree: root
  * @func: function
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
 
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
