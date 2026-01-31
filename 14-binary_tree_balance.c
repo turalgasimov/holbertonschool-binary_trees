e84c0f291adbffae46c8b8d45c4d91a11bf978f0
turalgasimov <turallgasimov@gmail.com>
2026-01-31 11:00:45 +0000

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - entry point
+ * @tree: root
+ *
+ * Return: height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t hl, hr;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (0);
+
+	hl = binary_tree_height(tree->left);
+	hr = binary_tree_height(tree->right);
+
+	return ((hl > hr ? hl : hr) + 1);
+}
+
+/**
  * binary_tree_balance - entry point
  * @tree: root
  *
@@ -13,16 +35,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (0);
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
 
-	lh = binary_tree_balance(tree->left);
-	rh = binary_tree_balance(tree->right);
-
-	if (lh == rh)
-		res = 1;
-	else
-		res = lh - rh;
-
-	return (res);
+	return (lh - rh);
 }
