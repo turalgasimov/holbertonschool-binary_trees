beb2684f2968190abee41d7d2b48c2d518b0b7dc
turalgasimov <turallgasimov@gmail.com>
2026-01-31 11:47:23 +0000

    15-binary_tree_is_full.c

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - entry point
+ * @tree: root
+ *
+ * Return: 1/0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int l, r;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left && tree->right)
+		return (1);
+
+	l = binary_tree_is_full(tree->left);
+	r = binary_tree_is_full(tree->right);
+
+	return (l && r);
+}
