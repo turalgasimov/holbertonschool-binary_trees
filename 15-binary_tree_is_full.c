39c7696987bb4428f8c5fb981daf228b8c9ab0eb
turalgasimov <turallgasimov@gmail.com>
2026-02-01 08:12:52 +0000

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,11 +13,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if ((tree->left && tree->right) || (!tree->left && !tree->right))
+	if (!tree->left && !tree->right)
 		return (1);
 
-	l = binary_tree_is_full(tree->left);
-	r = binary_tree_is_full(tree->right);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
-	return (l && r);
+	return (0);
 }
